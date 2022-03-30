2e203163cdcc9918aeb7e9153e30c435fcc93af2
Marcos Gómez <margobra8@gmail.com>
2022-03-30 17:57:18 +0200

    add more measurements

--- Core/Inc/preproc.h
+++ Core/Inc/preproc.h
@@ -1,7 +1,7 @@
 #define NO_SAMPLES_CONTROLLER 4001
-#define KP 17
-#define KI 1
-#define KD 10
+#define KP 90
+#define KI 2
+#define KD 2
 #define REV_ENC_CNT 3600
-#define CONTROLLER_TYPE_SEL PROPORTIONAL
+#define CONTROLLER_TYPE_SEL PID
 #define DELTA_TIME 0.001
