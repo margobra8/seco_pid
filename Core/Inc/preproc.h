211e782e4d6a7c10eda5e391a9eb88ec74f69326
Marcos Gómez <margobra8@gmail.com>
2022-03-30 18:33:01 +0200

    start fix of measurements from 0

--- Core/Inc/preproc.h
+++ Core/Inc/preproc.h
@@ -1,7 +1,7 @@
 #define NO_SAMPLES_CONTROLLER 4001
-#define KP 21
-#define KI 2
+#define KP 10
+#define KI 20
 #define KD 2
 #define REV_ENC_CNT 3600
-#define CONTROLLER_TYPE_SEL PROPORTIONAL
+#define CONTROLLER_TYPE_SEL INTEGRAL
 #define DELTA_TIME 0.001
